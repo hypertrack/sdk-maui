38f675845771a3f1c505ee92c690cd0a2f54cfd1
Pavel Kuznetsov <pavel@hypertrack.io>
2025-02-10 18:41:46 +0200

    Add common lib

---
+++ common/HyperTrackSdkMaui/HyperTrack.cs
@@ -0,0 +1,28 @@
+﻿namespace HyperTrack;
+
+#if ANDROID
+using Com.Hypertrack.Sdk.Android;
+#endif
+
+#if IOS
+using binding_ios;
+#endif
+
+public class HyperTrack
+{
+
+    // static var to get device id string
+    public static string DeviceId
+    {
+        get
+        {
+#if ANDROID
+            return Com.Hypertrack.Sdk.Android.HyperTrack.DeviceID + "android";
+#endif
+#if IOS
+            return HyperTrackMauiWrapper.DeviceId + "ios";
+#endif
+        }
+    }
+
+}
