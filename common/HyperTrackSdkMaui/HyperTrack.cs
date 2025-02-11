473a019af23cbf93c0a1ec3e377facbb6b43e708
Pavel Kuznetsov <pavel@hypertrack.io>
2025-02-11 19:26:13 +0200

    Get add geotag result from native

--- common/HyperTrackSdkMaui/HyperTrack.cs
+++ common/HyperTrackSdkMaui/HyperTrack.cs
@@ -1,26 +1,28 @@
 ﻿namespace HyperTrack;
 
 #if ANDROID
-using Com.Hypertrack.Sdk.Android;
+using HyperTrackAndroid = Com.Hypertrack.Sdk.Android.HyperTrack;
+using OrderStatusAndroid = Com.Hypertrack.Sdk.Android.HyperTrack.OrderStatus;
+using ClockInAndroid = Com.Hypertrack.Sdk.Android.HyperTrack.OrderStatus.ClockIn;
+using JsonAndroid = Com.Hypertrack.Sdk.Android.Json;
+using ResultAndroid = Com.Hypertrack.Sdk.Android.Result;
 #endif
 
 #if IOS
-using binding_ios;
+using HyperTrackIos = binding_ios.HyperTrackMauiWrapper;
 #endif
 
 public partial class HyperTrack
 {
-
-    // static var to get device id string
     public static string DeviceId
     {
         get
         {
 #if ANDROID
-            return Com.Hypertrack.Sdk.Android.HyperTrack.DeviceID + "android";
+            return HyperTrackAndroid.DeviceID;
 #endif
 #if IOS
-            return HyperTrackMauiWrapper.DeviceId + "ios";
+            return HyperTrackIos.DeviceId;
 #endif
         }
     }
@@ -30,7 +32,59 @@ public partial class HyperTrack
         OrderStatus orderStatus,
         Json.Object metadata)
     {
+#if ANDROID
+// public unsafe Object (global::System.Collections.Generic.IDictionary<string, global::Com.Hypertrack.Sdk.Android.Json> fields)
+// public unsafe Array (global::Java.Lang.IIterable items)
+//
+        OrderStatusAndroid orderStatusAndroid = ClockInAndroid.Instance;
+        JsonAndroid.Object metadataAndroid = FromJsonSharp(metadata) as JsonAndroid.Object;
+        ResultAndroid result = HyperTrackAndroid.AddGeotag(orderHandle, orderStatusAndroid, metadataAndroid);
+        return FromResultAndroid<HyperTrack.Location, HyperTrack.LocationError>(result);
+#endif
+#if IOS
         return Result<Location, LocationError>.Ok(new Location(0.0, 0.0));
+#endif
+    }
+
+    private static JsonAndroid FromJsonSharp(Json jsonSharp)
+    {
+        switch (jsonSharp)
+        {
+            case Json.Null:
+                return JsonAndroid.Null.Instance;
+            case Json.Bool b:
+                return new JsonAndroid.Bool(b.Value);
+            case Json.Number n:
+                return new JsonAndroid.Number(n.Value);
+            case Json.String s:
+                return new JsonAndroid.String(s.Value);
+            case Json.Array a:
+                JsonAndroid[] arr = a.Items.Select(FromJsonSharp).ToArray();
+                Java.Util.ArrayList list = new Java.Util.ArrayList(arr);
+                return new JsonAndroid.Array(list);
+            case Json.Object o:
+                return new JsonAndroid.Object(o.Fields.ToDictionary(
+                    kvp => kvp.Key,
+                    kvp => FromJsonSharp(kvp.Value)
+                ));
+            default:
+                throw new InvalidOperationException("Invalid Json value");
+        }
+    }
+
+    private static Result<T, E> FromResultAndroid<T, E>(ResultAndroid resultAndroid)
+        where T : class
+        where E : class
+    {
+        switch (resultAndroid)
+        {
+            case ResultAndroid.Success s:
+                return Result<T, E>.Ok(s.GetSuccess() as T);
+            case ResultAndroid.Failure f:
+                return Result<T, E>.Error(f.GetFailure() as E);
+            default:
+                throw new InvalidOperationException("Invalid Result value");
+        }
     }
 
 }
