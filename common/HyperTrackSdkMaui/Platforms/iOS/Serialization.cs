c2219e2f4a59d80e08e559c63cefc8687064b5a3
Pavel Kuznetsov <pavel@hypertrack.io>
2025-02-13 17:58:27 +0200

    wip

--- common/HyperTrackSdkMaui/Platforms/iOS/Serialization.cs
+++ common/HyperTrackSdkMaui/Platforms/iOS/Serialization.cs
@@ -5,37 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using Foundation;
 
+// We don't use private modifier because it's way easier to navigate this file when you have only alphabetical order
+// and we don't have the hassle to maintain the visibility consistency for all functions
 internal static class Serialization
 {
-    internal static Dictionary<string, object?> SerializeGeotagData(
-        HyperTrack.Json.Object metadata,
-        string orderHandle,
-        HyperTrack.OrderStatus orderStatus
-    )
-    {
-        return new Dictionary<string, object?>
-        {
-            { "metadata", metadata.ToDictionary() },
-            { "orderHandle", SerializeOrderHandle(orderHandle) },
-            { "orderStatus", SerializeOrderStatus(orderStatus) }
-        };
-    }
-
-    public static Dictionary<string, object?> SerializeLocation(HyperTrack.Location location)
-    {
-        return new()
-            {
-                { KEY_TYPE, TYPE_LOCATION },
-                { KEY_VALUE, new Dictionary<string, object?>
-                    {
-                        { KEY_LATITUDE, location.Latitude },
-                        { KEY_LONGITUDE, location.Longitude }
-                    }
-                }
-            };
-    }
-
-    private static HyperTrack.Location DeserializeLocation(Dictionary<string, object?> serialized)
+    internal static HyperTrack.Location DeserializeLocation(Dictionary<string, object?> serialized)
     {
         if (serialized[KEY_TYPE] as string != TYPE_LOCATION)
         {
@@ -53,9 +27,106 @@ internal static class Serialization
 
         return new HyperTrack.Location(latitude, longitude);
     }
-        
 
-    private static Dictionary<string, object?> SerializeOrderHandle(string orderHandle)
+    internal static HyperTrack.LocationResult DeserializeLocationResult(Dictionary<string, object?> serialized)
+    {
+        var type = serialized[KEY_TYPE] as string;
+        var value = serialized[KEY_VALUE] as Dictionary<string, object?>;
+
+        if (value == null)
+        {
+            throw new ArgumentException("Invalid LocationResult value: " + serialized);
+        }
+
+        if (type == TYPE_RESULT_SUCCESS)
+        {
+            return new HyperTrack.LocationResult.Success(DeserializeLocation(value));
+        }
+        else if (type == TYPE_RESULT_FAILURE)
+        {
+            // Assuming LocationError is handled similarly to Kotlin implementation
+            return new HyperTrack.LocationResult.Failure(DeserializeLocationError(value));
+        }
+
+        throw new ArgumentException("Invalid LocationResult type: " + type);
+    }
+
+    internal static HyperTrack.LocationError DeserializeLocationError(Dictionary<string, object?> serialized)
+    {
+        var type = serialized[KEY_TYPE] as string;
+
+        return type switch
+        {
+            TYPE_LOCATION_ERROR_NOT_RUNNING => new HyperTrack.LocationError.NotRunning(),
+            TYPE_LOCATION_ERROR_STARTING => new HyperTrack.LocationError.Starting(),
+            TYPE_LOCATION_ERROR_ERRORS => new HyperTrack.LocationError.Errors(
+                DeserializeErrors((serialized[KEY_VALUE] as List<Dictionary<string, object?>>)!)
+            ),
+            _ => throw new ArgumentException("Unknown location error type: " + type)
+        };
+    }
+
+    internal static HashSet<HyperTrack.Error> DeserializeErrors(List<Dictionary<string, object?>> errors)
+    {
+        return new HashSet<HyperTrack.Error>(
+            errors.Select(error => DeserializeError(error))
+        );
+    }
+
+    internal static HyperTrack.Error DeserializeError(Dictionary<string, object?> serialized)
+    {
+        var value = serialized[KEY_VALUE] as string;
+        if (value == null)
+        {
+            throw new ArgumentException("Invalid error value: " + serialized);
+        }
+
+        return value switch
+        {
+            "blockedFromRunning" => HyperTrack.Error.BlockedFromRunning,
+            "invalidPublishableKey" => HyperTrack.Error.InvalidPublishableKey,
+            "location.mocked" => HyperTrack.Error.Location.Mocked,
+            "location.servicesDisabled" => HyperTrack.Error.Location.ServicesDisabled,
+            "location.servicesUnavailable" => HyperTrack.Error.Location.ServicesUnavailable,
+            "location.signalLost" => HyperTrack.Error.Location.SignalLost,
+            "noExemptionFromBackgroundStartRestrictions" => HyperTrack.Error.NoExemptionFromBackgroundStartRestrictions,
+            "permissions.location.denied" => HyperTrack.Error.Permissions.Location.Denied,
+            "permissions.location.insufficientForBackground" => HyperTrack.Error.Permissions.Location.InsufficientForBackground,
+            "permissions.location.reducedAccuracy" => HyperTrack.Error.Permissions.Location.ReducedAccuracy,
+            "permissions.notifications.denied" => HyperTrack.Error.Permissions.Notifications.Denied,
+            _ => throw new ArgumentException("Unknown error value: " + value)
+        };
+    }
+
+    internal static Dictionary<string, object?> SerializeGeotagData(
+        HyperTrack.Json.Object metadata,
+        string orderHandle,
+        HyperTrack.OrderStatus orderStatus
+    )
+    {
+        return new Dictionary<string, object?>
+        {
+            { "metadata", metadata.ToDictionary() },
+            { "orderHandle", SerializeOrderHandle(orderHandle) },
+            { "orderStatus", SerializeOrderStatus(orderStatus) }
+        };
+    }
+
+    internal static Dictionary<string, object?> SerializeLocation(HyperTrack.Location location)
+    {
+        return new()
+            {
+                { KEY_TYPE, TYPE_LOCATION },
+                { KEY_VALUE, new Dictionary<string, object?>
+                    {
+                        { KEY_LATITUDE, location.Latitude },
+                        { KEY_LONGITUDE, location.Longitude }
+                    }
+                }
+            };
+    }
+        
+    internal static Dictionary<string, object?> SerializeOrderHandle(string orderHandle)
     {
         return new Dictionary<string, object?>
         {
@@ -64,7 +135,7 @@ internal static class Serialization
         };
     }
 
-    private static Dictionary<string, object?> SerializeOrderStatus(HyperTrack.OrderStatus orderStatus)
+    internal static Dictionary<string, object?> SerializeOrderStatus(HyperTrack.OrderStatus orderStatus)
     {
         return orderStatus switch
         {
@@ -102,4 +173,10 @@ internal static class Serialization
     private const string TYPE_GEOTAG_ORDER_STATUS_CLOCK_IN = "orderStatusClockIn";
     private const string TYPE_GEOTAG_ORDER_STATUS_CLOCK_OUT = "orderStatusClockOut";
     private const string TYPE_GEOTAG_ORDER_STATUS_CUSTOM = "orderStatusCustom";
+
+    private const string TYPE_RESULT_SUCCESS = "success";
+    private const string TYPE_RESULT_FAILURE = "failure";
+    private const string TYPE_LOCATION_ERROR_NOT_RUNNING = "notRunning";
+    private const string TYPE_LOCATION_ERROR_STARTING = "starting";
+    private const string TYPE_LOCATION_ERROR_ERRORS = "errors";
 }
